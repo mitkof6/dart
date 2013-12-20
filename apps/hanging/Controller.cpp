29f4566842b4c0333df60df21b284bde848499a9
Jeongseok Lee <jslee02@gmail.com>
2013-12-20 08:20:37 -0500

    Add hanging app

--- apps/hanging/Controller.cpp
+++ apps/hanging/Controller.cpp
@@ -1,65 +1,154 @@
-#include "Controller.h"
+/*
+ * Copyright (c) 2011-2013, Georgia Tech Research Corporation
+ * All rights reserved.
+ *
+ * Author(s): Karen Liu <karenliu@cc.gatech.edu>
+ *
+ * Geoorgia Tech Graphics Lab and Humanoid Robotics Lab
+ *
+ * Directed by Prof. C. Karen Liu and Prof. Mike Stilman
+ * <karenliu@cc.gatech.edu> <mstilman@cc.gatech.edu>
+ *
+ * This file is provided under the following "BSD-style" License:
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the following
+ *   conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#include "math/Helpers.h"
-#include "dynamics/Skeleton.h"
-#include "dynamics/BodyNode.h"
-#include "dynamics/GenCoord.h"
-#include "dynamics/Shape.h"
-#include "constraint/ConstraintDynamics.h"
-#include "collision/CollisionDetector.h"
+#include "apps/hanging/Controller.h"
 
-using namespace std;
-using namespace Eigen;
+#include "dart/math/Helpers.h"
+#include "dart/dynamics/Skeleton.h"
+#include "dart/dynamics/BodyNode.h"
+#include "dart/dynamics/GenCoord.h"
+#include "dart/dynamics/Shape.h"
+#include "dart/constraint/ConstraintDynamics.h"
+#include "dart/collision/CollisionDetector.h"
 
-using namespace dart;
-using namespace dynamics;
-using namespace math;
+Controller::Controller(dart::dynamics::Skeleton* _skel,
+                       dart::constraint::ConstraintDynamics* _constraintHandle,
+                       double _t) {
+  mSkel = _skel;
+  mConstraintHandle = _constraintHandle;
+  mTimestep = _t;
+  mFrame = 0;
+  int nDof = mSkel->getNumGenCoords();
+  mKp = Eigen::MatrixXd::Identity(nDof, nDof);
+  mKd = Eigen::MatrixXd::Identity(nDof, nDof);
+  mConstrForces = Eigen::VectorXd::Zero(nDof);
 
-Controller::Controller(dynamics::Skeleton* _skel, constraint::ConstraintDynamics* _constraintHandle, double _t) {
-    mSkel = _skel;
-    mConstraintHandle = _constraintHandle;
-    mTimestep = _t;
-    mFrame = 0;
-    int nDof = mSkel->getNumGenCoords();
-    mKp = MatrixXd::Identity(nDof, nDof);
-    mKd = MatrixXd::Identity(nDof, nDof);
-    mConstrForces = VectorXd::Zero(nDof);
-        
-    mTorques.resize(nDof);
-    mDesiredDofs.resize(nDof);
-    for (int i = 0; i < nDof; i++){
-        mTorques[i] = 0.0;
-        mDesiredDofs[i] = mSkel->getGenCoord(i)->get_q();
-    }
+  mTorques.resize(nDof);
+  mDesiredDofs.resize(nDof);
+  for (int i = 0; i < nDof; i++)
+  {
+    mTorques[i] = 0.0;
+    mDesiredDofs[i] = mSkel->getGenCoord(i)->get_q();
+  }
 
-    // using SPD results in simple Kp coefficients
-    for (int i = 0; i < 6; i++) {
-        mKp(i, i) = 0.0;
-        mKd(i, i) = 0.0;
-    }
-    for (int i = 6; i < 22; i++)
-        mKp(i, i) = 200.0; // lower body + lower back
-    for (int i = 22; i < nDof; i++)
-        mKp(i, i) = 20.0;
-    for (int i = 6; i < 22; i++) 
-        mKd(i, i) = 100.0;
-    for (int i = 22; i < nDof; i++) 
-        mKd(i, i) = 10.0;
-        
-    mPreOffset = 0.0;
+  // using SPD results in simple Kp coefficients
+  for (int i = 0; i < 6; i++)
+  {
+    mKp(i, i) = 0.0;
+    mKd(i, i) = 0.0;
+  }
+  for (int i = 6; i < 22; i++)
+    mKp(i, i) = 200.0; // lower body + lower back
+  for (int i = 22; i < nDof; i++)
+    mKp(i, i) = 20.0;
+  for (int i = 6; i < 22; i++)
+    mKd(i, i) = 100.0;
+  for (int i = 22; i < nDof; i++)
+    mKd(i, i) = 10.0;
+
+//  for (int i = 6; i < 22; i++)
+//    mKp(i, i) = 2.0; // lower body + lower back
+//  for (int i = 22; i < nDof; i++)
+//    mKp(i, i) = 2.0;
+//  for (int i = 6; i < 22; i++)
+//    mKd(i, i) = 2.0;
+//  for (int i = 22; i < nDof; i++)
+//    mKd(i, i) = 2.0;
+
+  mPreOffset = 0.0;
 }
 
-void Controller::computeTorques(const VectorXd& _dof, const VectorXd& _dofVel) {
-    // SPD tracking
-    int nDof = mSkel->getNumGenCoords();
-    MatrixXd invM = (mSkel->getMassMatrix() + mKd * mTimestep).inverse();
-    VectorXd p = -mKp * (_dof + _dofVel * mTimestep - mDesiredDofs);
-    VectorXd d = -mKd * _dofVel;
-    VectorXd qddot = invM * (-mSkel->getCombinedVector() + p + d + mConstrForces);
-    mTorques = p + d - mKd * qddot * mTimestep;
+Controller::~Controller()
+{
+}
 
-    for (int i = 0; i < 6; i++){        
-        mTorques[i] = 0.0;
-    }
-    mFrame++;
+Eigen::VectorXd Controller::getTorques()
+{
+  return mTorques;
+}
+
+double Controller::getTorque(int _index)
+{
+  return mTorques[_index];
+}
+
+void Controller::setDesiredDof(int _index, double _val)
+{
+  mDesiredDofs[_index] = _val;
+}
+
+void Controller::computeTorques(const Eigen::VectorXd& _dof,
+                                const Eigen::VectorXd& _dofVel)
+{
+  // SPD tracking
+  int nDof = mSkel->getNumGenCoords();
+  Eigen::MatrixXd invM = (mSkel->getMassMatrix() + mKd * mTimestep).inverse();
+  Eigen::VectorXd p = -mKp * (_dof + _dofVel * mTimestep - mDesiredDofs);
+  Eigen::VectorXd d = -mKd * _dofVel;
+  Eigen::VectorXd qddot
+      = invM * (-mSkel->getCombinedVector() + p + d + mConstrForces);
+  mTorques = p + d - mKd * qddot * mTimestep;
+
+  for (int i = 0; i < 6; i++)
+    mTorques[i] = 0.0;
+
+  mFrame++;
+}
+
+dart::dynamics::Skeleton*Controller::getSkeleton()
+{
+  return mSkel;
+}
+
+Eigen::VectorXd Controller::getDesiredDofs()
+{
+  return mDesiredDofs;
+}
+
+Eigen::MatrixXd Controller::getKp()
+{
+  return mKp;
+}
+
+Eigen::MatrixXd Controller::getKd()
+{
+  return mKd;
+}
+
+void Controller::setConstrForces(const Eigen::VectorXd& _constrForce)
+{
+  mConstrForces = _constrForce;
 }
--- apps/hanging/MyWindow.cpp
+++ apps/hanging/MyWindow.cpp
@@ -1,203 +1,239 @@
-#include "MyWindow.h"
+/*
+ * Copyright (c) 2011-2013, Georgia Tech Research Corporation
+ * All rights reserved.
+ *
+ * Author(s): Karen Liu <karenliu@cc.gatech.edu>
+ *
+ * Geoorgia Tech Graphics Lab and Humanoid Robotics Lab
+ *
+ * Directed by Prof. C. Karen Liu and Prof. Mike Stilman
+ * <karenliu@cc.gatech.edu> <mstilman@cc.gatech.edu>
+ *
+ * This file is provided under the following "BSD-style" License:
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the following
+ *   conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#include <stdio.h>
+#include "apps/hanging/MyWindow.h"
 
-#include "common/Timer.h"
-#include "math/Helpers.h"
-#include "collision/CollisionDetector.h"
-#include "constraint/ConstraintDynamics.h"
-#include "constraint/PointConstraint.h"
-#include "dynamics/BodyNode.h"
-#include "dynamics/GenCoord.h"
-#include "dynamics/Skeleton.h"
-#include "yui/GLFuncs.h"
+#include <cstdio>
 
-using namespace std;
-using namespace Eigen;
+#include "dart/common/Timer.h"
+#include "dart/math/Helpers.h"
+#include "dart/collision/CollisionDetector.h"
+#include "dart/constraint/ConstraintDynamics.h"
+#include "dart/constraint/PointConstraint.h"
+#include "dart/dynamics/BodyNode.h"
+#include "dart/dynamics/GenCoord.h"
+#include "dart/dynamics/Skeleton.h"
+#include "dart/simulation/World.h"
+#include "dart/gui/GLFuncs.h"
 
-using namespace dart;
-using namespace math;
-using namespace dynamics;
-using namespace constraint;
-using namespace yui;
-
-MyWindow::MyWindow(simulation::World* _world)
-    : SimWindow()
+MyWindow::MyWindow(dart::simulation::World* _world)
+  : SimWindow()
 {
-    assert(_world);
-    mWorld = _world;
+  SimWindow::setWorld(_world);
 
-    mForce = Eigen::Vector3d::Zero();
-    mImpulseDuration = 0;
+  mForce = Eigen::Vector3d::Zero();
+  mImpulseDuration = 0;
 
-    Eigen::Vector3d gravity(0.0, -9.81, 0.0);
-    mWorld->setGravity(gravity);
+  Eigen::Vector3d gravity(0.0, -9.81, 0.0);
+  mWorld->setGravity(gravity);
 
-    //
-    std::vector<int> genCoordIds0;
-    genCoordIds0.push_back(4);
+  //
+  std::vector<int> genCoordIds0;
+  genCoordIds0.push_back(4);
 
-    // default standing pose
-    std::vector<int> genCoordIds1;
-    genCoordIds1.push_back(1);
-    genCoordIds1.push_back(6); // left hip
-    genCoordIds1.push_back(9); // left knee
-    genCoordIds1.push_back(10); // left ankle
-    genCoordIds1.push_back(13); // right hip
-    genCoordIds1.push_back(16); // right knee
-    genCoordIds1.push_back(17); // right ankle
-    genCoordIds1.push_back(21); // lower back
-    genCoordIds1.push_back(28); // left shoulder
-    genCoordIds1.push_back(34); // right shoulder
+  // default standing pose
+  std::vector<int> genCoordIds1;
+  genCoordIds1.push_back(1);
+  genCoordIds1.push_back(6);  // left hip
+  genCoordIds1.push_back(14); // left knee
+  genCoordIds1.push_back(17); // left ankle
+  genCoordIds1.push_back(9);  // right hip
+  genCoordIds1.push_back(15); // right knee
+  genCoordIds1.push_back(19); // right ankle
+  genCoordIds1.push_back(13); // lower back
+  genCoordIds1.push_back(29); // left shoulder
+  genCoordIds1.push_back(32); // right shoulder
 
-    Eigen::VectorXd initConfig0(1);
-    initConfig0 << -2.9;
-    Eigen::VectorXd initConfig1(10);
-    initConfig1 << -0.1, 0.2, -0.5, 0.3, 0.2, -0.5, 0.3, -0.1, 0.5, -0.5;
+  Eigen::VectorXd initConfig0(1);
+  initConfig0 << -2.9;
+  Eigen::VectorXd initConfig1(10);
+  initConfig1 << -0.1, 0.2, -0.5, 0.3, 0.2, -0.5, 0.3, -0.1, 0.5, -0.5;
 
-    mWorld->getSkeleton(0)->setConfig(genCoordIds0, initConfig0);
-    mWorld->getSkeleton(1)->setConfig(genCoordIds1, initConfig1);
+  mWorld->getSkeleton(0)->setConfig(genCoordIds0, initConfig0);
+  mWorld->getSkeleton(1)->setConfig(genCoordIds1, initConfig1);
 
-    // create controller
-    mController = new Controller(mWorld->getSkeleton(1),
-                                 mWorld->getConstraintHandler(),
-                                 mWorld->getTimeStep());
+  // create controller
+  mController = new Controller(mWorld->getSkeleton(1),
+                               mWorld->getConstraintHandler(),
+                               mWorld->getTimeStep());
 
-    for (int i = 0; i < mWorld->getSkeleton(1)->getNumGenCoords(); i++)
-        mController->setDesiredDof(i, mController->getSkeleton()->getGenCoord(i)->get_q());
+  for (int i = 0; i < mWorld->getSkeleton(1)->getNumGenCoords(); i++)
+    mController->setDesiredDof(i, mController->getSkeleton()->getGenCoord(i)->get_q());
 
-    // initialize constraint on the hand
-    BodyNode* bd = mWorld->getSkeleton(1)->getBodyNode("h_hand_left");
-    PointConstraint* point1 = new PointConstraint(bd, bd->getLocalCOM(), bd->getWorldTransform().translation() + bd->getWorldCOM(), 1);
-    //mWorld->getConstraintHandler()->addConstraint(point1);
-    bd = mWorld->getSkeleton(1)->getBodyNode("h_hand_right");
-    PointConstraint* point2 = new PointConstraint(bd, bd->getLocalCOM(), bd->getWorldCOM(), 1);
-    //mWorld->getConstraintHandler()->addConstraint(point2);
+  // initialize constraint on the hand
+  dart::dynamics::BodyNode* bd
+      = mWorld->getSkeleton(1)->getBodyNode("h_hand_left");
+  dart::constraint::PointConstraint* point1
+      = new dart::constraint::PointConstraint(
+          bd, bd->getLocalCOM(), bd->getWorldCOM(), 1);
+  mWorld->getConstraintHandler()->addConstraint(point1);
+  bd = mWorld->getSkeleton(1)->getBodyNode("h_hand_right");
+  dart::constraint::PointConstraint* point2
+      = new dart::constraint::PointConstraint(
+          bd, bd->getLocalCOM(), bd->getWorldCOM(), 1);
+  mWorld->getConstraintHandler()->addConstraint(point2);
 }
 
 MyWindow::~MyWindow()
 {
 }
 
-void MyWindow::timeStepping(int _val)
+void MyWindow::timeStepping()
 {
-    int numIter = mDisplayTimeout / (mWorld->getTimeStep() * 1000);
-    if (mPlay)
-    {
-        mPlayFrame += 30;
-        if (mPlayFrame >= mBakedStates.size())
-            mPlayFrame = 0;
-        glutPostRedisplay();
-        glutTimerFunc(mDisplayTimeout, refreshTimer, _val);
-    }
-    else if (mSimulating)
-    {
-        mWorld->step();
+  mWorld->getSkeleton(1)->getBodyNode("h_pelvis")->addExtForce(mForce);
 
-        // for perturbation test
-        mImpulseDuration--;
-        if (mImpulseDuration <= 0) {
-            mImpulseDuration = 0;
-            mForce.setZero();
-        }
-//        glutPostRedisplay();
-//        glutTimerFunc(mDisplayTimeout, refreshTimer, _val);
-    }
+  mController->setConstrForces(
+        mWorld->getConstraintHandler()->getTotalConstraintForce(1));
+  mController->computeTorques(mWorld->getSkeleton(1)->get_q(),
+                              mWorld->getSkeleton(1)->get_dq());
+  mWorld->getSkeleton(1)->setInternalForceVector(mController->getTorques());
+
+  mWorld->step();
+
+  // for perturbation test
+  mImpulseDuration--;
+  if (mImpulseDuration <= 0)
+  {
+    mImpulseDuration = 0;
+    mForce.setZero();
+  }
 }
 
 void MyWindow::drawSkels()
 {
-    for (unsigned int i = 0; i < mWorld->getNumSkeletons(); i++)
-        mWorld->getSkeleton(i)->draw(mRI);
+  for (unsigned int i = 0; i < mWorld->getNumSkeletons(); i++)
+    mWorld->getSkeleton(i)->draw(mRI);
 
-    // draw handholds
-    mRI->setPenColor(Vector3d(0.2, 0.2, 0.2));
-    mRI->pushMatrix();
-    glTranslated(0.0, -0.06, -0.52);
-    mRI->drawEllipsoid(Vector3d(0.1, 0.1, 0.1));
-    mRI->popMatrix();
-    mRI->setPenColor(Vector3d(0.2, 0.2, 0.2));
-    mRI->pushMatrix();
-    glTranslated(0.0, -0.06, 0.52);
-    mRI->drawEllipsoid(Vector3d(0.1, 0.1, 0.1));
-    mRI->popMatrix();
+  // draw handholds
+  mRI->setPenColor(Eigen::Vector3d(0.2, 0.2, 0.2));
+  mRI->pushMatrix();
+  glTranslated(0.0, -0.06, -0.52);
+  mRI->drawEllipsoid(Eigen::Vector3d(0.1, 0.1, 0.1));
+  mRI->popMatrix();
+  mRI->setPenColor(Eigen::Vector3d(0.2, 0.2, 0.2));
+  mRI->pushMatrix();
+  glTranslated(0.0, -0.06, 0.52);
+  mRI->drawEllipsoid(Eigen::Vector3d(0.1, 0.1, 0.1));
+  mRI->popMatrix();
 
-    // draw arrow
-    if (mImpulseDuration > 0) {
-        Vector3d poa = mWorld->getSkeleton(1)->getBodyNode("fullbody1_root")->getWorldTransform() * Vector3d(0.0, 0.0, 0.0);
-        Vector3d start = poa - mForce / 10.0;
-        double len = mForce.norm() / 10.0;
-        drawArrow3D(start, mForce, len, 0.05, 0.1);
-    }
+  // draw arrow
+  if (mImpulseDuration > 0)
+  {
+    dart::dynamics::BodyNode* bodyNode
+        = mWorld->getSkeleton(1)->getBodyNode("h_pelvis");
+    Eigen::Vector3d poa = bodyNode->getWorldTransform().translation();
+    Eigen::Vector3d start = poa - mForce / 10.0;
+    double len = mForce.norm() / 10.0;
+    dart::gui::drawArrow3D(start, mForce, len, 0.05, 0.1);
+  }
 }
 
 void MyWindow::keyboard(unsigned char key, int x, int y)
 {
-    switch(key){
-    case ' ': // use space key to play or stop the motion
-        mSimulating = !mSimulating;
-        if (mSimulating) {
-            mPlay = false;
-            glutTimerFunc( mDisplayTimeout, refreshTimer, 0);
-        }
-        break;
-    case 's': // simulate one frame
-        if (!mPlay) {
-            mForce = Vector3d::Zero();
-            mWorld->step();
-            bake();
-            glutPostRedisplay();
-        }
-        break;
+  switch(key)
+  {
+    case ' ':  // use space key to play or stop the motion
+      mSimulating = !mSimulating;
+      if (mSimulating)
+      {
+        mPlay = false;
+        glutTimerFunc( mDisplayTimeout, refreshTimer, 0);
+      }
+      break;
+    case 's':  // simulate one frame
+      if (!mPlay)
+      {
+        mForce = Eigen::Vector3d::Zero();
+        mWorld->step();
+        bake();
+        glutPostRedisplay();
+      }
+      break;
     case '1':
-        mForce[0] = 20;
-        mImpulseDuration = 10.0;
-        cout << "push forward" << endl;
-        break;
+      mForce[0] = 20;
+      mImpulseDuration = 10.0;
+      std::cout << "push forward" << std::endl;
+      break;
     case '2':
-        mForce[0] = -10;
-        mImpulseDuration = 10.0;
-        cout << "push backward" << endl;
-        break;
+      mForce[0] = -20;
+      mImpulseDuration = 10.0;
+      std::cout << "push backward" << std::endl;
+      break;
     case '3':
-        mForce[2] = 50;
-        mImpulseDuration = 10.0;
-        cout << "push right" << endl;
-        break;
+      mForce[2] = 50;
+      mImpulseDuration = 10.0;
+      std::cout << "push right" << std::endl;
+      break;
     case '4':
-        mForce[2] = -50;
-        mImpulseDuration = 10.0;
-        cout << "push left" << endl;
-        break;
-    case 'p': // playBack
-        mPlay = !mPlay;
-        if (mPlay) {
-            mSimulating = false;
-            glutTimerFunc( mDisplayTimeout, refreshTimer, 0);
-        }
-        break;
-    case '[': // step backward
-        if (!mSimulating) {
-            mPlayFrame--;
-            if(mPlayFrame < 0)
-                mPlayFrame = 0;
-            glutPostRedisplay();
-        }
-        break;
-    case ']': // step forwardward
-        if (!mSimulating) {
-            mPlayFrame++;
-            if(mPlayFrame >= mBakedStates.size())
-                mPlayFrame = 0;
-            glutPostRedisplay();
-        }
-        break;
-    case 'v': // show or hide markers
-        mShowMarkers = !mShowMarkers;
-        break;
+      mForce[2] = -50;
+      mImpulseDuration = 10.0;
+      std::cout << "push left" << std::endl;
+      break;
+    case 'p':  // playBack
+      mPlay = !mPlay;
+      if (mPlay)
+      {
+        mSimulating = false;
+        glutTimerFunc( mDisplayTimeout, refreshTimer, 0);
+      }
+      break;
+    case '[':  // step backward
+      if (!mSimulating)
+      {
+        mPlayFrame--;
+        if(mPlayFrame < 0)
+          mPlayFrame = 0;
+        glutPostRedisplay();
+      }
+      break;
+    case ']':  // step forwardward
+      if (!mSimulating)
+      {
+        mPlayFrame++;
+        if(mPlayFrame >= mBakedStates.size())
+          mPlayFrame = 0;
+        glutPostRedisplay();
+      }
+      break;
+    case 'v':  // show or hide markers
+      mShowMarkers = !mShowMarkers;
+      break;
 
     default:
-        Win3D::keyboard(key,x,y);
-    }
-    glutPostRedisplay();
+      Win3D::keyboard(key,x,y);
+  }
+  glutPostRedisplay();
 }
